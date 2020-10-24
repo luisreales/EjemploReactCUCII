3f8e512401319eedb06831e49687305bdf8e50f9
LUISREALES <48936231+luisreales@users.noreply.github.com>
2020-10-24 12:23:45 -0500

    Updates

---
+++ precache-manifest.51d34f101ea62fdeed378b44ea36db20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454fcd311eafa667e1bb97342e06cb88",
+    "url": "/luisreales/EjemploReactCUCII/index.html"
+  },
+  {
+    "revision": "5220a51aa9d29ecbb0a8",
+    "url": "/luisreales/EjemploReactCUCII/static/css/main.cb892542.chunk.css"
+  },
+  {
+    "revision": "afa0a05188c6cdced0fe",
+    "url": "/luisreales/EjemploReactCUCII/static/js/2.5b7a3fe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luisreales/EjemploReactCUCII/static/js/2.5b7a3fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5220a51aa9d29ecbb0a8",
+    "url": "/luisreales/EjemploReactCUCII/static/js/main.01b072b1.chunk.js"
+  },
+  {
+    "revision": "c51113d3d48833906f99",
+    "url": "/luisreales/EjemploReactCUCII/static/js/runtime-main.c35db98a.js"
+  }
+]);
