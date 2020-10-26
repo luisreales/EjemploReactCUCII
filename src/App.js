442b38c38d2f791be61f5b822ca42e08f2bd23a8
LUISREALES <48936231+luisreales@users.noreply.github.com>
2020-10-26 09:25:01 -0500

    add remove in the car item

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Mensaje from './components/Mensaje';
 import Contador from './components/Contador';
 import {ContadorHook} from './components/ContadorHook';
 import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 
 function App() {
   const fecha = new Date().getFullYear();
@@ -17,22 +18,30 @@ function App() {
     {id:4,nombre:'Camisa Real Cartagena',precio:500},
   ])
 
+  //state para un carrito de compra
+  const[carrito,agregarProducto] = useState([]);
+
+
+
   return (
     <Fragment>
          <Header titulo='Tienda Virtual El Carmelo' /> 
-         <Mensaje size='30px' color='#ff0000' contenido='AHORA' />
+         {/* <Mensaje size='30px' color='#ff0000' contenido='AHORA' />
          <Mensaje size='40px' color='#00ff00' contenido='ENTIENDO' />
          <Mensaje size='60px' color='#0000ff' contenido='LOS PROPS :D' />
          <Contador /> 
-         <ContadorHook /> 
+         <ContadorHook />  */}
          {/* <Mensaje size='30px' color='#ff0000' contenido='AHORA' />
          <Mensaje size='40px' color='#00ff00' contenido='ENTIENDO' />
          <Mensaje size='60px' color='#0000ff' contenido='LOS PROPS :D' />
          <Contador /> 
          <ContadorHook /> */}
+         <h1>Lista de Productos</h1>
          {productos.map(productoItem => (
-           <Producto producto={productoItem} key={productoItem.id} />
+           <Producto producto={productoItem} key={productoItem.id} carrito={carrito} agregarProducto={agregarProducto} productos={productos}/>
          ))}
+
+         <Carrito carrito={carrito} agregarProducto={agregarProducto}/>
 
 
          <Footer fecha={fecha}/>
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,44 @@
 import React from 'react';
-const producto = ({producto}) => {
+const producto = ({producto,carrito,agregarProducto,productos}) => {
 
     const {nombre, precio, id} = producto;
+
+    //agregar producto al carrito
+    const selecionarProducto = id =>{
+        const producto = productos.filter(producto => producto.id === id )[0];
+                agregarProducto([
+                    ...carrito, 
+                    producto
+                ]);
+    }
+    const eliminarProducto = id =>{
+        // const productos = carrito.filter(producto => producto.id !== id);
+        // agregarProducto([
+        //     ...carrito,
+        //     productos
+        // ])
+        const productos = carrito.filter(producto => producto.id !== id);
+
+        // Colocar los productos en el state
+        agregarProducto(productos)
+
+    }
     return (
         <div>
             <h2>{nombre}</h2>
             <h2>${precio}</h2>            
+            {productos
+                ?
+                (
+                    <button type="button" id="comprar" onClick={() => selecionarProducto(id)}>Comprar</button>
+                )
+                :
+                (
+                    <button type="button" id="comprar" onClick={() => eliminarProducto(id)}>Eliminar</button>
+                )
+            }
+            
+            
         </div>
     
         
@@ -14,3 +47,55 @@ const producto = ({producto}) => {
 }
  
 export default producto;
+
+// import React from 'react';
+
+// const Producto = ({producto, carrito, agregarProducto, productos}) => {
+
+
+//     const { nombre, precio, id } = producto;
+
+//     // Agregar producto al carrito
+//     const seleccionarProducto = id => {
+//         const producto = productos.filter(producto => producto.id === id )[0];
+//         agregarProducto([
+//             ...carrito, 
+//             producto
+//         ]);
+//     }
+
+//     // Elimina un producto del carrito
+//     const eliminarProducto = id => {
+//         const productos = carrito.filter(producto => producto.id !== id);
+
+//         // Colocar los productos en el state
+//         agregarProducto(productos)
+//     }
+
+//     return ( 
+//         <div>
+//             <h2>{nombre}</h2>
+//             <p>${precio}</p>
+            
+//             { productos 
+//             ?
+//                 (
+//                     <button 
+//                         type="button"
+//                         onClick={ () => seleccionarProducto(id) }
+//                     >Comprar</button>
+//                 )
+//             : 
+//                 (
+//                     <button 
+//                         type="button"
+//                         onClick={ () => eliminarProducto(id) }
+//                     >Eliminar</button>
+//                 )
+//             }
+//         </div>
+//      );
+// }
+
+ 
+// export default Producto;
